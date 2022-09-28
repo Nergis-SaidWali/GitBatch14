8d6ccb6b6b4644edf4df302fe3fa9eabfb28e04f
Nergis-SaidWali <nergis.879018@gmail.com>
2022-09-28 03:06:57 +0300

    third commit

--- src/class03B14/class03.java
+++ src/class03B14/class03.java
@@ -5,8 +5,8 @@ public class class03 {
 	public static void main(String[] args) {
 		
 		System.out.println("hey this is a git class");
-		System.out.println("hey this is a change in mu project");
-
+		System.out.println("hey this is a change in my project");
+		System.out.println("how are you this is a new version");
 	}
 
 }
